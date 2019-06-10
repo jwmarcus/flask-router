716335ec47f29ca5c9b49cb7e283d493511d5976
John Marcus III <1401764+jwmarcus@users.noreply.github.com>
2019-06-10 17:50:57 -0400

    adding basic in and out routes

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
   return 'Hello, World!'
+
+@app.route('/log/<device_id>', methods=['GET', 'POST'])
+def log(device_id):
+    if request.method == 'POST':
+        return '{}\'s endpoint sent data'.format(device_id)
+    else:
+        return '{}\'s endpoint requested data'.format(device_id)
